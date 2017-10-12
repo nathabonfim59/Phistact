4669c24ff7dae9eafc1936b42432a2dae4682a34
nathabonfim59 <{username}@users.noreply.github.com>
2017-10-12 19:41:31 +0000

    Sidebar concluída

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+function activate_button(id) {
+    // Adiciona a aparência de botão selectionado
+    document.getElementById(id).style.backgroundColor = '#9C528B';
+    document.getElementById(id).style.color = '#fff';
+
+    // Remove a seleção do elemento anterior
+    if (id === 'add-users') {
+        document.getElementById('list-users').style.backgroundColor = 'rgba(0, 0, 0, 0)';
+        document.getElementById('list-users').style.color = '#676A95';
+    } else {
+        document.getElementById('add-users').style.backgroundColor = 'rgba(0, 0, 0, 0)';
+        document.getElementById('add-users').style.color = '#676A95';
+    }
+}
