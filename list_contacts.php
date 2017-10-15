f1422399c8ab0d95e30bb9a9c4f2f5e461dfaaae
nathabonfim59 <nathabonfim59@users.noreply.github.com>
2017-10-15 04:32:42 +0000

    Fixed frase: sem contatos

--- list_contacts.php
+++ list_contacts.php
@@ -37,6 +37,6 @@
         $contact_list = json_decode($_COOKIE['contact_list'], true);
         display_contacts($contact_list);
     } else {
-        echo '<h1 style="text-align: center;padding-top: 30px;">Ainda não há nenhum contato salvo.<br><br>Adicione um clicando no botão <strong>Adicionar contato</strong> a esquerda.</h1>';
+        echo '<h1 style="text-align: center;padding-top: 30px;">Ainda não há nenhum contato salvo.<br><br>Adicione um clicando no botão "Adicionar contato" a esquerda.</h1>';
     }
 ?>
